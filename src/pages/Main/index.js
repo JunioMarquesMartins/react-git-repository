0a93a9821f52fb74868973c61a3e739890984125
Junio Marques <jmarques@CDM-230.home>
2020-03-23 23:38:17 +0100

    filter issues for status and add pagination

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -14,6 +14,7 @@ export default class Main extends Component {
     newRepo: '',
     repositories: [],
     loading: false,
+    error: null,
   };
 
   // Load localstorage data
@@ -34,31 +35,40 @@ export default class Main extends Component {
   }
 
   handleInputChange = e => {
-    this.setState({ newRepo: e.target.value });
+    this.setState({ newRepo: e.target.value, error: null });
   }
 
   handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ loading: true });
+    this.setState({ loading: true, error: false });
 
-    const { newRepo, repositories } = this.state;
 
-    const response = await api.get(`/repos/${newRepo}`);
+    try {
+      const { newRepo, repositories } = this.state;
+      const response = await api.get(`/repos/${newRepo}`);
+      // repositorie existing
+      const hasRepo = repositories.find(r => r.name === newRepo);
+      if (hasRepo) throw 'Repositório duplicado';
 
-    const data = {
-      name: response.data.full_name,
+      const data = {
+        name: response.data.full_name,
+      }
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+      });
+    } catch(error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
     }
 
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-      loading: false,
-    });
+    
   }
 
   render() {
-    const { newRepo, repositories, loading } = this.state;
+    const { newRepo, repositories, loading, error } = this.state;
     return (
       <Container>
           <h1>
@@ -66,7 +76,7 @@ export default class Main extends Component {
               Repositórios
           </h1>
   
-          <Form onSubmit={this.handleSubmit}>
+          <Form onSubmit={this.handleSubmit} error={error}>
             <input
               type="text"
               placeholder="Adicionar repositório"
